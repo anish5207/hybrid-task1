e4562a8f20a38cfb1923d33bd29d890afee9780b
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-06-15 11:52:37 +0530

    fourth

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <?php
 <body bgcolor=Aqua>
 <h2 style="color:red" align=center> HEY WELCOME, THIS IS MY FIRST WEB PAGE CREATED BY TERRAFORM </h2>
-<img src="https://task1-anish-bucket.s3.ap-south-1.amazonaws.com/image.jpg" height="300px" width="500px" >
+<img src="https://task1-anish-bucket.s3.ap-south-1.amazonaws.com/image.jpg" height="300px" width="500px"/>
 ?>
 </pre>
