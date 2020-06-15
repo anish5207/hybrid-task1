27a26dca1ecec27639e8e4ceace6196ddc29c445
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-06-15 14:08:53 +0530

    trj

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<pre>
-
-HEY WELCOME, THIS IS MY FIRST WEB PAGE CREATED BY TERRAFORM
-<img src="https://skabhi-01.s3.ap-south-1.amazonaws.com/image.jpg" height="300px" width="500px"/>
-<?php
-print `ifconfig`;
-?>
-</pre>
+<html>
+<head>
+<title>LW India</title>
+</head>
+<body>
+  <h1>Mr Vimal Daga #RightEducation</h1>
+<img src=""https://task1-anish-bucket.s3.ap-south-1.amazonaws.com/practical.jpg"" width="800" height="550" style=" margin:10px 250px">
+</body>
+</html>
