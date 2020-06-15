fbefd0400caf794454b2ff1636cad0048cc1402b
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-06-15 14:09:53 +0530

    gfhhg

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>LW India</title>
 </head>
 <body>
-  <h1>Mr Vimal Daga #RightEducation</h1>
+  <h1>Web page</h1>
 <img src=""https://task1-anish-bucket.s3.ap-south-1.amazonaws.com/practical.jpg"" width="800" height="550" style=" margin:10px 250px">
 </body>
 </html>
