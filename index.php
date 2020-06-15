a244f8371b621c6b9fdbb4ea7eeb161d7d083272
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-06-15 13:48:53 +0530

    sasa

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <pre>
+
+HEY WELCOME, THIS IS MY FIRST WEB PAGE CREATED BY TERRAFORM
+<img src="https://skabhi-01.s3.ap-south-1.amazonaws.com/image.jpg" height="300px" width="500px"/>
 <?php
-<h2 style="color:red" align=center> HEY WELCOME, THIS IS MY FIRST WEB PAGE CREATED BY TERRAFORM </h2>
-<img src="https://task1-anish-bucket.s3.ap-south-1.amazonaws.com/image.jpg" height="300px" width="500px"/>
+print `ifconfig`;
 ?>
 </pre>
