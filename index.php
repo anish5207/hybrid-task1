495703c13073e73f0d8f851ccaa46894c53a88c6
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-07-16 22:20:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>LW India</title>
 </head>
 <body style="background-color:black;">
-  <h1>This web page is created by terraform</h>
+  <h1>This web page is created by terraform</h1>
   <p><p><b><font size="8" color="white">WELCOME TO THE WORLD OF HYBRID CLOUD COMPUTING.</font></b></p></p>
 </body>
 </html>
