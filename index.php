b8d74a6fad5e939149287fa0594a0a722d351580
anish5207 <64491832+anish5207@users.noreply.github.com>
2020-07-16 17:49:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <head>
 <title>LW India</title>
 </head>
-<body>
+<body style="background-color:black;">
   <h1>This web page is created by terraform</h1>
-  <img src="task1-anish-bucket.s3.ap-south-1.amazonaws.com/practical.jpg">
+  <img src="task1-anish-bucket.s3.ap-south-1.amazonaws.com/practical.jpg"  width="300" height="150">
+  <p><p><b><font size="8" color="white">WELCOME TO THE WORLD OF HYBRID CLOUD COMPUTING.</font></b></p></p>
 </body>
 </html>
